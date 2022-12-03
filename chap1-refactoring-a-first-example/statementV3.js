f92a8acbd300549a86ce78953447a5cf2a0a2993
Wang Shenghao <shenghao.wang@gojek.com>
2022-12-03 13:29:14 +0800

    Display statement in HTML

--- chap1-refactoring-a-first-example/statementV3.js
+++ chap1-refactoring-a-first-example/statementV3.js
@@ -19,16 +19,32 @@ function renderPlainText(data) {
   result += `You earned ${data.totalVolumeCredits} credits\n`;
 
   return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber/100);
-  }
 }
 
+window.htmlStatement = function htmlStatement() {
+  return renderHtml(createStatementData(invoice, plays));
+}
 
-const res = statement(invoice, plays);
-console.log(res)
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber/100);
+}
